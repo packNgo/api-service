e127ec74960226451a3725c644cecdf2c89eef8e
Ilia Kamilov <iliakmlv@gmail.com>
2020-10-18 02:06:25 +0300

    Added endpoints for API

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Express from 'express'
 import { config } from 'dotenv'
+import * as HikingAPI from './services/Hiking'
 
 config()
 
@@ -8,6 +9,18 @@ const app: Express.Application = Express()
 const PORT: string | number = process.env.PORT || 4000
 
 app.use(Express.json())
+
+app.get('/trails', async (req: Express.Request, res: Express.Response) => {
+    const hiking = await HikingAPI.getTrailsByCordinates(req.query)
+
+    res.status(200).json([{hiking}])
+})
+
+app.get('/trails/:ids', async (req: Express.Request, res: Express.Response) => {
+    const hiking = await HikingAPI.getTrailByIds(req.params.ids.split(','))
+
+    res.status(200).json({ hiking})
+})
 
 app.listen(PORT, (): void => {
     console.log(`listening *:${PORT}`)
